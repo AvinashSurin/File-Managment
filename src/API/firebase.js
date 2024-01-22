b3c244b7ba3bed6b32466f1084eda88c7091c4af
Avinash Surin <88780535+AvinashSurin@users.noreply.github.com>
2024-01-22 23:32:33 +0530

    Update firebase.js

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.React_App_apiKey,
-  authDomain: process.env.React_App_authDomain,
-  projectId: process.env.React_App_projectId,
-  storageBucket: process.env.React_App_storageBucket,
-  messagingSenderId: process.env.React_App_messagingSenderId,
-  appId: process.env.React_App_appId,
+  apiKey: process.env.VITE_apiKey,
+  authDomain: process.env.VITE_authDomain,
+  projectId: process.env.VITE_projectId,
+  storageBucket: process.env.VITE_storageBucket,
+  messagingSenderId: process.env.VITE_messagingSenderId,
+  appId: process.env.VITE_appId,
 };
 
 firebase.initializeApp(firebaseConfig);
